c74bae67db2be94ab1fc0f8aab86a59ccdbfe060
Shannon Poole <shannon.m.poole@gmail.com>
2014-05-13 12:43:36 -0400

    testing fig.yml

---
+++ server_2/README.md
@@ -0,0 +1,10 @@
+webhook-test
+============
+
+This is a test of GitHub Webhooks and Modit
+
+
+TEST
+====
+
+Testing tar directory and empty file issues
---
+++ server_2/server.js
@@ -0,0 +1,27 @@
+var express     = require('express')
+  , app         = express()
+  , mongoose    = require('mongoose')
+  , fs          = require('fs');
+  
+app.get('/hello.txt', function(req, res){
+  res.send('Hello World:' + process.env.TEST);
+});
+
+app.get('/empty', function(req, res){
+  var response = '';
+  
+  if (fs.existsSync('empty')) {
+    response += 'directory "empty" exists <br>'
+    // Do something
+  }
+  if (fs.existsSync('empty.txt')) {
+    response += 'file "empty.txt" exists'
+    // Do something
+  }
+  
+  res.send(response);
+});
+
+var server = app.listen(process.env.WEB_PORT || 80, function() {
+  console.log('Listening on port %d', server.address().port);
+});
